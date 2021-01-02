0a0dae52d74f4dcc7ce5886811446f66342590ec
“Drmingler” <kayymedia@gmail.com>
2021-01-02 15:54:27 +0100

    feat: Add styles to NavBar component

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,6 @@
-import React  from "react";
+import React, {useEffect} from "react";
 import "../assets/scss/style.scss";
+import { StylesProvider } from "@material-ui/core/styles";
 import Header from "../components/Header";
 import Cities from "../components/Cities";
 import Steps from "../components/Steps";
@@ -8,19 +9,30 @@ import AvailableCities from "../components/AvailableCities";
 import ListingCategories from "../components/ListingCategories";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import MobileNavbar from "../components/MobileNavBar";
 
 function App() {
+  useEffect(()=>{
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  },[])
   return (
-    <div className="App">
-      <NavBar />
-      <Header />
-      <Cities />
-      <Steps />
-      <PerfectPlace />
-      <AvailableCities />
-      <ListingCategories />
-      <Footer />
-    </div>
+    <StylesProvider injectFirst>
+      <div className="App">
+        <MobileNavbar />
+        <NavBar />
+        <Header />
+        <Cities />
+        <Steps />
+        <PerfectPlace />
+        <AvailableCities />
+        <ListingCategories />
+        <Footer />
+      </div>
+    </StylesProvider>
   );
 }
 
