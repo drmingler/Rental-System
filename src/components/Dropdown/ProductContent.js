7afeda3fa4bfb3933480fc0dae6932ed8f606d51
“Drmingler” <kayymedia@gmail.com>
2021-01-02 15:43:43 +0100

    feat: Add ProductContent component

---
+++ src/components/Dropdown/ProductContent.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import { DropdownItem } from "./index";
+import { ReactComponent as HouseIcon } from "../../assets/house.svg";
+
+const ProductContent = () => {
+  return (
+    <Fragment>
+      <h3>Rental Accommodation</h3>
+      <DropdownItem icon={<HouseIcon />}>
+        <h5>Find a place</h5>
+        <span>How renting works on our platform</span>
+      </DropdownItem>
+      <DropdownItem icon={<HouseIcon />}>
+        <h5>Rent out your place</h5>
+        <span>How to rent your properties online</span>
+      </DropdownItem>
+    </Fragment>
+  );
+};
+export default ProductContent;
