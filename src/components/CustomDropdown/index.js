92bcc433bdc59bd38762ae7425dd9c150fae46d5
“Drmingler” <kayymedia@gmail.com>
2021-01-02 15:45:19 +0100

    feat: Add MobileLearnMore component

--- src/components/CustomDropdown/index.js
+++
@@ -1,4 +0,0 @@
-import MenuOptionsDropDown from "./MenuOptionsDropDown";
-import DropdownItem from "./DropDownItem";
-import AccountSettingsDropDown from "./AccountSettingsDropDown";
-export { MenuOptionsDropDown, DropdownItem, AccountSettingsDropDown };
---
+++ src/components/MobileDropDown/MobileLearnMore.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  MobileDropDownHeader,
+  MobileDropDownWrapper,
+  MobileMenuOptionDropDown
+} from "./index";
+import { LearnMoreContent } from "../Dropdown";
+
+const MobileLearnMore = () => {
+  return (
+    <MobileDropDownWrapper>
+      <MobileDropDownHeader>
+        <span>Learn More</span>
+      </MobileDropDownHeader>
+      <MobileMenuOptionDropDown height={150}>
+        <div className="mobile-menu-option-content-container">
+          <LearnMoreContent />
+        </div>
+      </MobileMenuOptionDropDown>
+    </MobileDropDownWrapper>
+  );
+};
+export default MobileLearnMore;
