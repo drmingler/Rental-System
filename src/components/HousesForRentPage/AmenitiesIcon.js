eddde221b30876fbaf5885a3f6ee557fcf344fc4
“Drmingler” <kayymedia@gmail.com>
2021-02-16 06:10:13 +0100

    fix: AmenitiesIcon clicking

--- src/components/HousesForRentPage/AmenitiesIcon.js
+++ src/components/HousesForRentPage/AmenitiesIcon.js
@@ -1,24 +1,37 @@
 import React from "react";
-import {Col} from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
-const AmenitiesIcon = ({ iconDark, iconWhite, text, filtersOptions, setFilterOption }) => {
-    let filterOption = filtersOptions[text];
+const AmenitiesIcon = ({
+  iconDark,
+  iconWhite,
+  text,
+  filtersOptions,
+  setFilterOption
+}) => {
+  let filterOption = filtersOptions[text];
 
-    function handleClick() {
-        if (filterOption) {
-            const { [filterOption]: _, ...newFilterOptions } = filtersOptions;
-            setFilterOption(newFilterOptions);
-        } else {
-            setFilterOption({ ...filtersOptions, [text]: text });
-        }
+  function handleClick() {
+    if (filterOption) {
+      const { [filterOption]: _, ...newFilterOptions } = filtersOptions;
+      setFilterOption(newFilterOptions);
+    } else {
+      setFilterOption({ ...filtersOptions, [text]: text });
     }
+  }
 
-    return (
-        <Col xs={6} className="panel-icon-container" onClick={handleClick}>
-            <div className={`panel-icon ${filterOption && "active"}`}>{filterOption ? iconWhite : iconDark}</div>
-            <span className="panel-icon-text">{text}</span>
-        </Col>
-    );
+  return (
+    <Col xs={6} className="panel-icon-container">
+      <div
+        className={`panel-icon ${filterOption && "active"}`}
+        onClick={handleClick}
+      >
+        {filterOption ? iconWhite : iconDark}
+      </div>
+      <span className="panel-icon-text" onClick={handleClick}>
+        {text}
+      </span>
+    </Col>
+  );
 };
 
 export default AmenitiesIcon;
