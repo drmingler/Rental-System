d95ba742d30115cf9c7d1353a4d9c96d88437164
“Drmingler” <kayymedia@gmail.com>
2021-02-16 06:00:29 +0100

    feat: Export amenities SVG icons

---
+++ src/assets/img/amenities/index.js
@@ -0,0 +1,83 @@
+import { ReactComponent as PoolDark } from "./gray-colour/pool.svg";
+import { ReactComponent as PoolWhite } from "./white-colour/pool.svg";
+import { ReactComponent as GardenDark } from "./gray-colour/garden.svg";
+import { ReactComponent as GardenWhite } from "./white-colour/garden.svg";
+import { ReactComponent as ElevatorDark } from "./gray-colour/elevator.svg";
+import { ReactComponent as ElevatorWhite } from "./white-colour/elevator.svg";
+import { ReactComponent as DoormanDark } from "./gray-colour/doorman.svg";
+import { ReactComponent as DoormanWhite } from "./white-colour/doorman.svg";
+import { ReactComponent as DeckDark } from "./gray-colour/deck.svg";
+import { ReactComponent as DeckWhite } from "./white-colour/deck.svg";
+import { ReactComponent as WasherDark } from "./gray-colour/washer.svg";
+import { ReactComponent as WasherWhite } from "./white-colour/washer.svg";
+import { ReactComponent as GymDark } from "./gray-colour/dumbbell.svg";
+import { ReactComponent as GymWhite } from "./white-colour/dumbbell.svg";
+import { ReactComponent as ParkingDark } from "./gray-colour/parking.svg";
+import { ReactComponent as ParkingWhite } from "./white-colour/parking.svg";
+import { ReactComponent as FireplaceDark } from "./gray-colour/fire-place.svg";
+import { ReactComponent as FireplaceWhite } from "./white-colour/fire-place.svg";
+import { ReactComponent as AirConditionDark } from "./gray-colour/air-conditioning.svg";
+import { ReactComponent as AirConditionWhite } from "./white-colour/air-conditioning.svg";
+import { ReactComponent as DishwasherDark } from "./gray-colour/dishwasher.svg";
+import { ReactComponent as DishwasherWhite } from "./white-colour/dishwasher.svg";
+import { ReactComponent as StorageDark } from "./gray-colour/storage.svg";
+import { ReactComponent as StorageWhite } from "./white-colour/storage.svg";
+import { ReactComponent as WheelchairDark } from "./gray-colour/wheelchair-accessible.svg";
+import { ReactComponent as WheelchairWhite } from "./white-colour/wheelchair-accessible.svg";
+import { ReactComponent as BalconyDark } from "./gray-colour/balcony.svg";
+import { ReactComponent as BalconyWhite } from "./white-colour/balcony.svg";
+import { ReactComponent as HardFloorDark } from "./gray-colour/hard-floor.svg";
+import { ReactComponent as HardFloorWhite } from "./white-colour/hard-floor.svg";
+import { ReactComponent as FurnishedDark } from "./gray-colour/furnished.svg";
+import { ReactComponent as FurnishedWhite } from "./white-colour/furnished.svg";
+import { ReactComponent as ViewDark } from "./gray-colour/view.svg";
+import { ReactComponent as ViewWhite } from "./white-colour/view.svg";
+import { ReactComponent as HighRiseDark } from "./gray-colour/high-rise.svg";
+import { ReactComponent as HighRiseWhite } from "./white-colour/high-rise.svg";
+import { ReactComponent as StudentFriendlyDark } from "./gray-colour/student-friendly.svg";
+import { ReactComponent as StudentFriendlyWhite } from "./white-colour/student-friendly.svg";
+import { ReactComponent as UtilitiesDark } from "./gray-colour/utilities-included.svg";
+import { ReactComponent as UtilitiesWhite } from "./white-colour/utilities-included.svg";
+
+export {
+  PoolDark,
+  PoolWhite,
+  GardenDark,
+  GardenWhite,
+  ElevatorDark,
+  ElevatorWhite,
+  DoormanDark,
+  DoormanWhite,
+  DeckDark,
+  DeckWhite,
+  WasherDark,
+  WasherWhite,
+  GymDark,
+  GymWhite,
+  ParkingDark,
+  ParkingWhite,
+  FireplaceDark,
+  FireplaceWhite,
+  AirConditionDark,
+  AirConditionWhite,
+  DishwasherDark,
+  DishwasherWhite,
+  StorageDark,
+  StorageWhite,
+  WheelchairDark,
+  WheelchairWhite,
+  BalconyDark,
+  BalconyWhite,
+  HardFloorDark,
+  HardFloorWhite,
+  FurnishedDark,
+  FurnishedWhite,
+  ViewDark,
+  ViewWhite,
+  HighRiseDark,
+  HighRiseWhite,
+  StudentFriendlyDark,
+  StudentFriendlyWhite,
+  UtilitiesDark,
+  UtilitiesWhite
+};
