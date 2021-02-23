11fc8fa21e5b6d2f2879c0edaf2bb0b2c8af9a05
“Drmingler” <kayymedia@gmail.com>
2021-02-23 14:37:30 +0100

    feat: Add landlord contact card

--- src/pages/HouseDetailsPage.js
+++ src/pages/HouseDetailsPage.js
@@ -7,6 +7,9 @@ import {
   HouseImagesSection,
   HouseAmenitiesSection
 } from "../components/HouseDetailsPage/index";
+import { Container, Row, Col } from "react-bootstrap";
+import Paper from "@material-ui/core/Paper";
+import HouseImage from "../assets/img/property-image-1.jpg";
 
 const HouseDetailsPage = () => {
   return (
@@ -17,6 +20,35 @@ const HouseDetailsPage = () => {
       <HouseImagesSection />
       <HouseDescriptionSection />
       <HouseAmenitiesSection />
+      <section className="contact-landlord-section">
+        <Container>
+          <Row>
+            <Col md={9}>
+              <Row>
+                <Col md={4}>
+                  <Paper variant="outlined">
+                    <div className="contact-agent-card">
+                      <div className="landlord-image-container">
+                        <img src={HouseImage} alt="landlord" />
+                      </div>
+                    </div>
+                    <div className="card-content">
+                      <div className="landlord-short-details">
+                        <h3>Stefanie</h3>
+                        <span>From Berlin, Germany</span>
+                      </div>
+                      <button className="contact-landlord-button">
+                        Contact Stefanie
+                      </button>
+                    </div>
+                  </Paper>
+                </Col>
+                <Col md={8}></Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </Fragment>
   );
 };
