a144ac28870f1a14542053f5bcc3cbec35dfc788
“Drmingler” <kayymedia@gmail.com>
2021-02-26 07:12:16 +0100

    feat: Make apply for property container sticky

--- src/components/HouseDetailsPage/Header.js
+++ src/components/HouseDetailsPage/Header.js
@@ -1,16 +1,15 @@
-import React, {useEffect, useState} from "react";
-import {Col, Container, Row} from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import Paper from "@material-ui/core/Paper";
 import CustomTransition from "../CustomTransition";
 import HouseImage from "../../assets/img/0be3acfcb6d1bd77327fee16dbe221877d7e314a.jpg";
-import {ReactComponent as ArrowDownRight} from "../../assets/img/arrow-right-circle.svg";
+import { ReactComponent as ArrowDownRight } from "../../assets/img/arrow-right-circle.svg";
 import MoneyIconWhite from "../../assets/img/amenities/white-colour/money-icon-white.svg";
 import BedIconWhite from "../../assets/img/amenities/white-colour/bed-icon-white.svg";
 import CatIconWhite from "../../assets/img/amenities/white-colour/cat-icon-white.svg";
 import DogIconWhite from "../../assets/img/amenities/white-colour/dog-icon-white.svg";
 import SqrtIconWhite from "../../assets/img/amenities/white-colour/sqrt-icon-white.svg";
 import BathIconWhite from "../../assets/img/amenities/white-colour/bath-icon-white.svg";
-
 
 const Utilities = ({ icon, value, name }) => {
   return (
@@ -39,7 +38,7 @@ const Header = () => {
         finalClass={"animate"}
         style={{ backgroundImage: `url(${HouseImage})` }}
       />
-      <Container className="property-header-container" fluid={'lg'}>
+      <Container className="property-header-container" fluid={"lg"}>
         <Row>
           <Col sm={12} className="property-header-address">
             <div className="address-wrapper apartment">
@@ -76,24 +75,24 @@ const Header = () => {
                   </ul>
                 </div>
               </Col>
-              <Col sm={3}>
-                <Paper elevation={1} className="apply-for-property">
-                  <div className="property-monthly-price">
-                    <div className="monthly-price-header">
-                      <span>Monthly Price</span>
-                    </div>
-                    <div className="price">
-                      <ArrowDownRight className="arrow-icon" />
-                      <span>$1,500</span>
-                    </div>
-                  </div>
-                  <div className="property-availability">
-                    <span className="available">Available:</span>
-                    <span className="right-now">Right Now</span>
-                  </div>
-                  <button className="apply-for-property-button">Apply</button>
-                </Paper>
-              </Col>
+              {/*<Col sm={3}>*/}
+              {/*  <Paper elevation={1} className="apply-for-property">*/}
+              {/*    <div className="property-monthly-price">*/}
+              {/*      <div className="monthly-price-header">*/}
+              {/*        <span>Monthly Price</span>*/}
+              {/*      </div>*/}
+              {/*      <div className="price">*/}
+              {/*        <ArrowDownRight className="arrow-icon" />*/}
+              {/*        <span>$1,500</span>*/}
+              {/*      </div>*/}
+              {/*    </div>*/}
+              {/*    <div className="property-availability">*/}
+              {/*      <span className="available">Available:</span>*/}
+              {/*      <span className="right-now">Right Now</span>*/}
+              {/*    </div>*/}
+              {/*    <button className="apply-for-property-button">Apply</button>*/}
+              {/*  </Paper>*/}
+              {/*</Col>*/}
             </Row>
           </Col>
         </Row>
--- src/pages/HouseDetailsPage.js
+++ src/pages/HouseDetailsPage.js
@@ -10,17 +10,41 @@ import {
   ContactLandlordSection,
   SimilarPropertiesSection
 } from "../components/HouseDetailsPage/index";
+import Paper from "@material-ui/core/Paper";
+import { ReactComponent as ArrowDownRight } from "../assets/img/arrow-right-circle.svg";
 
 const HouseDetailsPage = () => {
   return (
     <Layout>
       <MobileNavbar />
       <NavBar fixed={true} />
-      <Header />
-      <HouseImagesSection />
-      <HouseDescriptionSection />
-      <HouseAmenitiesSection />
-      <ContactLandlordSection />
+      <div className="sticky-container">
+        <div className="sticky-apply-container">
+          <div className="sticky-apply sticky-active">
+            <Paper elevation={1} className="apply-for-property">
+              <div className="property-monthly-price">
+                <div className="monthly-price-header">
+                  <span>Monthly Price</span>
+                </div>
+                <div className="price">
+                  <ArrowDownRight className="arrow-icon" />
+                  <span>$1,500</span>
+                </div>
+              </div>
+              <div className="property-availability">
+                <span className="available">Available:</span>
+                <span className="right-now">Right Now</span>
+              </div>
+              <button className="apply-for-property-button">Apply</button>
+            </Paper>
+          </div>
+        </div>
+        <Header />
+        <HouseImagesSection />
+        <HouseDescriptionSection />
+        <HouseAmenitiesSection />
+        <ContactLandlordSection />
+      </div>
       <SimilarPropertiesSection />
     </Layout>
   );
